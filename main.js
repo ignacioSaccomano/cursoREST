0bf9dd16325ca2aa5a8daef7cd60b7e88d4b7ee8
Ignacio Saccomano <nachosacco1>
2023-03-01 03:27:05 -0300

    Meto imagenes dinamicas :)

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const API_URL = 'https://api.thedogapi.com/v1/images/search?api_key=live_fti1EGcHFCqEDBCI4oco9zzZZGClViWfMSK8CoDHNPo0oR5ZJvKmmi4hIMHgXt22'
-const imagen = document.querySelector('img')
+const imageWrapper = document.querySelector('.imagecontainer')
 const boton = document.getElementById('boton')
+const img = document.querySelectorAll('img')
+const number_input = document.getElementById('input-perros')
 
 // setInterval(() => {
 //     fetch(URL)
@@ -10,10 +12,20 @@ const boton = document.getElementById('boton')
 //         })
 // }, 2000)
 
-async function reload() {
+async function fetchImage() {
     const resultado = await fetch(API_URL)
     const data = await resultado.json()
-    imagen.src = data[0].url
+    return data[0].url
 }
 
-reload()
+function makeImages(amount) {
+    let foto = document.createElement('img')
+    fetchImage().then(el => foto.src = el)
+    imageWrapper.appendChild(foto)
+}
+
+number_input.addEventListener('input', (e)=>{
+    makeImages(e.target.valueAsNumber)
+})
+
+makeImages(1)
