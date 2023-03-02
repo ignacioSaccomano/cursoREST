4e8dcb68daf2488978519180613379649c2fc82d
Ignacio Saccomano <nachosacco1>
2023-03-02 17:44:26 -0300

    Cambio urls

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://api.thedogapi.com/v1/images/search?api_key=live_fti1EGcHFCqEDBCI4oco9zzZZGClViWfMSK8CoDHNPo0oR5ZJvKmmi4hIMHgXt22'
+const API_URL = 'https://api.thedogapi.com/v1/images/search'
+const API_KEY = 'api_key=live_fti1EGcHFCqEDBCI4oco9zzZZGClViWfMSK8CoDHNPo0oR5ZJvKmmi4hIMHgXt22'
 const imageWrapper = document.querySelector('.imagecontainer')
 const boton = document.getElementById('boton')
 const img = document.querySelectorAll('img')
